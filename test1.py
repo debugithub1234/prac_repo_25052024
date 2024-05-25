38fb518aa0d0022807c344a608a59106d097a567
debugithub1234 <debuwb.dutta@gmail.com>
2024-05-25 17:23:53 +0530

    my commit_25

---
+++ test1.py
@@ -0,0 +1,25 @@
+# new project
+
+print("new name")
+
+# add newthings
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+# My First push 1
+
+#My second push and see what happen
+
+# My third commit fro github to vc code it is a pull
+
+# fourth pull 
+
+# Fifth push
+
+# Sixth push
+
+# seventh push 
+
+# new six
